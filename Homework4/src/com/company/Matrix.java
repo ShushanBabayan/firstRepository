0dbca42cc1ed9c5dd5e46c10c7f0ebf0c086960d
ShushanBabayan <shushan.babayan00@gmail.com>
2020-09-09 03:26:38 +0400

    matrix operations

--- Homework4/src/com/company/Matrix.java
+++ Homework4/src/com/company/Matrix.java
@@ -3,7 +3,7 @@ package com.company;
 public class Matrix {
     private int rowNumber;
     private int columnNumber;
-    private int[][] matrix = new int[rowNumber][columnNumber];
+    private int[][] matrix;
 
     public Matrix(){
         super();
@@ -12,6 +12,7 @@ public class Matrix {
     public Matrix(int rowNumber,int columnNumber, int[][] matrix){
         this.rowNumber = rowNumber;
         this.columnNumber = columnNumber;
+        this.matrix = new int[rowNumber][columnNumber];
         for(int i = 0; i < rowNumber; ++i){
             for (int j = 0; j < columnNumber; ++j){
                 this.matrix[i][j] = matrix[i][j];
@@ -34,37 +35,47 @@ public class Matrix {
         return matrix[i][j];
     }
 
-    public Matrix addMatrices(Matrix first, Matrix second){
+    public static Matrix addMatrices(Matrix first, Matrix second){
         if(first.getRowNumber() != second.getRowNumber() || first.getColumnNumber() != second.getColumnNumber()){
             System.out.println("Matrices cannot be added");
-            Matrix newMatrix = new Matrix();
-            return newMatrix;
+            return new Matrix();
         }
-        int matrix[][] = new int[first.rowNumber][first.columnNumber];
+        int[][] matrix = new int[first.rowNumber][first.columnNumber];
         for(int i = 0; i < first.rowNumber; ++i){
             for (int j = 0; j < first.columnNumber; ++j){
                 matrix[i][j] = first.getElement(i, j) + second.getElement(i, j);
             }
         }
-        Matrix newMatrix = new Matrix(first.rowNumber, first.columnNumber, matrix);
-        return newMatrix;
+        return new Matrix(first.rowNumber, first.columnNumber, matrix);
     }
 
-    public Matrix multiplyMatrices(Matrix first, Matrix second){
+    public static Matrix multiplyMatrices(Matrix first, Matrix second){
         if(first.columnNumber != second.rowNumber){
             System.out.println("Matrices cannot be multiplied");
-            Matrix newMatrix = new Matrix();
-            return newMatrix;
+            return new Matrix();
         }
-        int matrix[][] = new int[first.rowNumber][first.columnNumber];
+        int[][] matrix = new int[first.rowNumber][second.columnNumber];
+        for (int i = 0; i < first.rowNumber; ++i) {
+            for (int j = 0; j < second.columnNumber; ++j) {
+                matrix[i][j] = 0;
+            }
+        }
         for (int i = 0; i < first.rowNumber; ++i){
             for (int j = 0; j < second.columnNumber; ++j){
                 for(int k = 0; k < first.columnNumber ; ++k){
-                    matrix[i][j] = first.getElement(i, k) * second.getElement(k, j);
+                    matrix[i][j] += first.getElement(i, k) * second.getElement(k, j);
                 }
             }
         }
-        Matrix newMatrix = new Matrix(first.rowNumber, second.columnNumber, matrix);
-        return newMatrix;
+        return new Matrix(first.rowNumber, second.columnNumber, matrix);
+    }
+
+    public void print(){
+        for(int i = 0; i < this.rowNumber; ++i){
+            for (int j = 0; j < this.columnNumber; ++j){
+                System.out.print(this.getElement(i, j) + " ");
+            }
+            System.out.println();
+        }
     }
 }
